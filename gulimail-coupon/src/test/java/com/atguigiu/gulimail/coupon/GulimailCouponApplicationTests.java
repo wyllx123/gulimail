76ec2969f1c5cbdf82a84ed9fae1ef719f62ae15
luoxu <1137492123@qq.com>
2022-10-25 19:34:09 +0800

    项目初始结构创建

---
+++ gulimail-coupon/src/test/java/com/atguigiu/gulimail/coupon/GulimailCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigiu.gulimail.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
